1c0e142d7b3c7c4e716b612cad6b7d24ff167260
Renfred Harper <renfredxh@gmail.com>
2015-02-05 03:46:03 -0500

    Add walking dog prototype based on starstruck example

--- js/main.js
+++ js/main.js
@@ -1,52 +1,117 @@
 window.onload = function() {
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/master/resources/Project%20Templates/Basic
-    
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
-    
-    "use strict";
-    
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
-    
-    function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, render: render });
+
+  function preload() {
+
+    game.load.tilemap('level', 'assets/testLevel.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('tiles', 'assets/tiles.png');
+    game.load.spritesheet('dog', 'assets/dogsheet.png', 128, 96);
+    game.load.spritesheet('droid', 'assets/droid.png', 32, 32);
+    game.load.image('starSmall', 'assets/star.png');
+    game.load.image('starBig', 'assets/star2.png');
+    game.load.image('background', 'assets/background2.png');
+
+  }
+
+  var map;
+  var tileset;
+  var layer;
+  var player;
+  var facing = 'right';
+  var jumpTimer = 0;
+  var cursors;
+  var jumpButton;
+  var bg;
+
+  function create() {
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    game.stage.backgroundColor = '#000000';
+
+    bg = game.add.tileSprite(0, 0, 800, 600, 'background');
+    bg.fixedToCamera = true;
+
+    map = game.add.tilemap('level');
+
+    map.addTilesetImage('tiles');
+
+    map.setCollisionByExclusion([]);
+
+    layer = map.createLayer('Level');
+
+    //  Un-comment this on to see the collision tiles
+    // layer.debug = true;
+
+    layer.resizeWorld();
+
+    game.physics.arcade.gravity.y = 980;
+
+    player = game.add.sprite(20, 3000, 'dog');
+    game.physics.enable(player, Phaser.Physics.ARCADE);
+
+    player.body.bounce.y = 0.2;
+    player.body.collideWorldBounds = true;
+    player.body.setSize(96, 64, 25, 32);
+
+    player.animations.add('right', [0, 1, 2, 3], 10, true);
+    player.animations.add('left', [6, 7, 8, 9], 10, true);
+    player.animations.add('turn', [4], 20, true);
+
+    game.camera.follow(player);
+
+    cursors = game.input.keyboard.createCursorKeys();
+    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+  }
+
+  function update() {
+
+    game.physics.arcade.collide(player, layer);
+
+    player.body.velocity.x = 0;
+
+    if (cursors.left.isDown) {
+      player.body.velocity.x = -150;
+
+      if (facing != 'left') {
+        player.animations.play('left');
+        facing = 'left';
+      }
+    } else if (cursors.right.isDown) {
+      player.body.velocity.x = 150;
+
+      if (facing != 'right') {
+        player.animations.play('right');
+        facing = 'right';
+      }
     }
-    
-    var bouncy;
-    
-    function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
-        
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something awesome.", style );
-        text.anchor.setTo( 0.5, 0.0 );
+    else {
+      if (facing != 'idle') {
+        player.animations.stop();
+
+        if (facing == 'left') {
+          player.frame = 5;
+        }
+        else {
+          player.frame = 0;
+        }
+
+        facing = 'idle';
+      }
     }
-    
-    function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
+
+    if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer) {
+      player.body.velocity.y = -400;
+      jumpTimer = game.time.now + 750;
     }
-};
+  }
+
+  function render () {
+
+    //game.debug.text(game.time.physicsElapsed, 32, 32);
+    //game.debug.body(player);
+    //game.debug.bodyInfo(player, 16, 24);
+
+  }
+}
