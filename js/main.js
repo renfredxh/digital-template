b2d5c8452740bb62ba28031e064c7e722a949a2b
Renfred Harper <renfredxh@gmail.com>
2015-02-05 08:31:27 -0500

    Add player health an enemy collisions

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
 "use strict"
 
 window.onload = function() {
-  function randInt(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  var enemyLocations = [[15,90], [30, 90], [35, 90]]
-
   var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, render: render });
 
   function preload() {
@@ -21,6 +15,7 @@ window.onload = function() {
 
   var map;
   var tileset;
+  var healthText;
   var layer;
   var player;
   var playerData = {};
@@ -29,14 +24,16 @@ window.onload = function() {
   var jumpButton;
   var bg;
 
+  var enemyLocations = [[15,90], [30, 90], [35, 90]]
+
   function create() {
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     game.stage.backgroundColor = '#000000';
 
-    bg = game.add.tileSprite(0, 0, 800, 600, 'background');
-    bg.fixedToCamera = true;
+    //bg = game.add.tileSprite(0, 0, 800, 600, 'background');
+    //bg.fixedToCamera = true;
 
     map = game.add.tilemap('level');
 
@@ -54,9 +51,11 @@ window.onload = function() {
     game.physics.arcade.gravity.y = 980;
 
     // Player
-    playerData.facing = 'right'
-    playerData.moving = false
-    playerData.jumpTimer = 0
+    playerData.facing = 'right';
+    playerData.moving = false;
+    playerData.health = 100;
+    playerData.jumpTimer = 0;
+    playerData.healthTimer = 0;
     player = game.add.sprite(20, 3000, 'dog');
     game.physics.enable(player, Phaser.Physics.ARCADE);
 
@@ -83,17 +82,21 @@ window.onload = function() {
       game.physics.enable(enemy, Phaser.Physics.ARCADE);
       enemy.body.bounce.y = 0.2;
       enemy.body.collideWorldBounds = true
-      enemy.body.setSize(64, 70);
+      enemy.body.setSize(52, 58, 7, 10);
       // Random movement pattern for each enemy
       var enemyVelocity = randInt(40, 60);
       var enemyDelay = randInt(0, 800)
       var enemyDuration = randInt(4000, 5000)
       enemy.body.velocity.x = -enemyVelocity;
       game.add.tween(enemy.body.velocity).to( {x: enemyVelocity}, enemyDuration, Phaser.Easing.Back.InOut, true, enemyDelay, false)
-
-      game.debug.body(enemy);
     }
 
+
+    healthText = game.add.text(720, 570, 'Health: ' + parseInt(playerData.health), { fontSize: '34px', fill: '#FFF'});
+    healthText.anchor.set(0.5)
+    healthText.fixedToCamera = true;
+    healthText.stroke = '#141414';
+    healthText.strokeThickness = 4;
 
     cursors = game.input.keyboard.createCursorKeys();
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
@@ -136,16 +139,16 @@ window.onload = function() {
 
     // Jumping
     if (jumpButton.isDown && player.body.onFloor() && game.time.now > playerData.jumpTimer) {
-      player.body.velocity.y = -400;
+      player.body.velocity.y = -420;
       playerData.jumpTimer = game.time.now + 750;
     }
     if (!player.body.onFloor()) {
-      if (player.body.velocity.y < -300) {
+      if (player.body.velocity.y < -200) {
         player.frame = {
           right: 10,
           left: 13
         }[playerData.facing]
-      } else if (player.body.velocity.y >= -300 && player.body.velocity.y < 25) {
+      } else if (player.body.velocity.y >= -200 && player.body.velocity.y < 25) {
         player.frame = {
           right: 12,
           left: 15
@@ -167,8 +170,8 @@ window.onload = function() {
   function render () {
     //game.debug.text(game.time.physicsElapsed, 32, 32);
     //game.debug.body(player);
-    game.debug.bodyInfo(player, 16, 24);
-    // enemies.forEach(function(enemy) { game.debug.body(enemy); });
+    //game.debug.bodyInfo(player, 16, 24);
+    //enemies.forEach(function(enemy) { game.debug.body(enemy); });
 
   }
 
@@ -182,6 +185,15 @@ window.onload = function() {
   }
 
   function enemyContact(player, enemy) {
-    console.log("Hit")
+    if (game.time.now > playerData.healthTimer) {
+      playerData.health -= 10;
+      player.body.velocity.y = -200;
+      healthText.text = 'Health: ' + playerData.health;
+      playerData.healthTimer = game.time.now + 1200;
+    }
+  }
+
+  function randInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 }
